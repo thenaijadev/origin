bc72f0100733af985770762f33bf69cabb733175
Okwor Chukwuemeka <thenaijadev@gmail.com>
2021-12-05 13:39:04 -0800

    complete weather webapp

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const https = require("https");
 const app = express();
+app.set("view engine", 'ejs');
+
 const bodyParser = require("body-parser");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 const axios = require("axios");
-const { response } = require("express");
+const {
+    response
+} = require("express");
 app.get("/", function (req, res) {
-   res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 
 });
 
@@ -17,17 +21,25 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
     const city = req.body.city;
     const url = `https://api.openweathermap.org/data/2.5/weather?appid=0a69c33a749fb857c93c946ccf19f681&units=metric&q=${city}`;
-    axios.get(url).then(response=>{
+    axios.get(url).then(response => {
         const weatherData = response.data;
         const temperature = weatherData.main.temp
-        console.log(temperature);
-       
+        const country = weatherData.sys.country;
+        const description = weatherData.weather[0].description
+        console.log(description);
+        res.render("weather", {
+            temperature: temperature,
+            city: city,
+            country: country,
+            description:description
+        });
+
+
     });
-    
+
 });
 
 app.listen("3000", function () {
     console.log("server is running on port 3000....")
 });
 
-// https://github.com/thenaijadev/origin.git
