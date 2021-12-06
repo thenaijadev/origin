ac1fded605f365e89bba28e026f1781ed0dd9f08
Okwor Chukwuemeka <thenaijadev@gmail.com>
2021-12-05 21:12:26 -0800

    ok

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.post("/", function (req, res) {
             temperature: temperature,
             city: city,
             country: country,
-            description:description
+            description: description
         });
 
 
@@ -39,7 +39,6 @@ app.post("/", function (req, res) {
 
 });
 
-app.listen("3000", function () {
+app.listen(process.env.PORT || "3000", function () {
     console.log("server is running on port 3000....")
 });
-
